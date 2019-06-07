45a527d5e57db66ba71ae1c40f684bb51fba929f
PailletJuanPablo <pailletjuanpablo@gmail.com>
2019-06-07 05:45:07 -0300

    Default page

--- app/Http/Controllers/MultimediaController.php
+++ app/Http/Controllers/MultimediaController.php
@@ -2,13 +2,15 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
 use App\Multimedia;
 use Storage;
 class MultimediaController extends Controller
 {
     public function getMultimedia($audio){
         $multimedia = Multimedia::find($audio);
+        if(!$multimedia){
+            return redirect('https://ddna.cba.gov.ar/');
+        }
         $multimedia->media_url = Storage::url($multimedia->media_url);
         $multimedia->currentUrl = url()->current();
         return view('detail', ['multimedia'=>$multimedia]);
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,8 +6,9 @@
     <div class="overlay"></div>
     <div class="container-fluid px-0">
         <div class="row d-md-flex no-gutters slider-text align-items-center js-fullheight justify-content-end">
-            <img class="one-third js-fullheight align-self-end order-md-last img-fluid"
-                src="images/undraw_pair_programming_njlp.svg" alt="">
+          <div class="imageContainer">
+          <img class="img-fluid" src="{{asset('custom/logo.png')}}">
+        </div>
             <div class="one-forth d-flex align-items-center ftco-animate js-fullheight">
                 <div class="text mt-5">
                     <span class="subheading">Bienvenido</span>
